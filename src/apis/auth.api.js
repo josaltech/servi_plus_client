c80493e7118ee5e99a02b39ba537a638111bc489
josaltech <josalgallego@gmail.com>
2022-11-17 23:16:18 -0500

    Change api urls to production heroku server

--- src/apis/auth.api.js
+++ src/apis/auth.api.js
@@ -1,6 +1,8 @@
-import axios from 'axios';
+import axios from "axios";
 
-const AUTH_API_URL = process.env.REACT_APP_AUTH_API_URL;
+const AUTH_API_URL =
+  "https://servi-plus-server.herokuapp.com/api/v1/auth" ||
+  process.env.REACT_APP_AUTH_API_URL;
 
 export const registerRequest = async (email, password) =>
   await axios.post(`${AUTH_API_URL}/register`, { email, password });
--- src/apis/tickets.api.js
+++ src/apis/tickets.api.js
@@ -1,10 +1,12 @@
-import axios from 'axios';
-const TICKETS_API_URL = process.env.REACT_APP_TICKETS_API_URL;
+import axios from "axios";
+const TICKETS_API_URL =
+  "https://servi-plus-server.herokuapp.com/api/v1/tickets" ||
+  process.env.REACT_APP_TICKETS_API_URL;
 console.log(TICKETS_API_URL);
 
 export const createTicketRequest = async (task, token) =>
   await axios({
-    method: 'POST',
+    method: "POST",
     url: TICKETS_API_URL,
     data: task,
     headers: {
@@ -14,7 +16,7 @@ export const createTicketRequest = async (task, token) =>
 
 export const getAllTicketsRequest = async (token) =>
   await axios({
-    method: 'GET',
+    method: "GET",
     url: TICKETS_API_URL,
     headers: {
       Authorization: `Bearer ${token}`,
@@ -31,7 +33,7 @@ export const getOneTicketRequest = async (id, token) =>
 
 export const updateTicketRequest = async (id, newFields, token) =>
   await axios({
-    method: 'PUT',
+    method: "PUT",
     url: `${TICKETS_API_URL}/${id}`,
     data: newFields,
     headers: {
@@ -41,7 +43,7 @@ export const updateTicketRequest = async (id, newFields, token) =>
 
 export const deleteTicketRequest = async (id, token) =>
   await axios({
-    method: 'DELETE',
+    method: "DELETE",
     url: `${TICKETS_API_URL}/${id}`,
     headers: {
       Authorization: `Bearer ${token}`,
